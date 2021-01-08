930773510d529c2eaea9d818f006028cd2e843b2
“Linda <linda.resseguier@gmail.com>
2021-01-08 15:07:17 +0100

    get/post routes for albums & tracks done

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to my favorite albums list");
 });
 
+// GET ALL ALBUMS
 app.get("/albums", (request, response) => {
   connection.query("SELECT * from albums", (err, results) => {
     if (err) {
@@ -30,6 +31,34 @@ app.get("/albums", (request, response) => {
   });
 });
 
+// GET ALL TRACKS
+app.get("/tracks", (request, response) => {
+  connection.query("SELECT * from tracks", (err, results) => {
+    if (err) {
+      response.status(500).send("Error retrieving albums");
+    } else {
+      response.status(200).json(results);
+    }
+  });
+});
+
+// GET ALBUMS BY ID
+app.get("/albums/:id", (req, res) => {
+  connection.query(
+    "SELECT * from albums WHERE id=?",
+    [req.params.id],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error retrieving albums");
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+// CREATE ALBUM(S)
 app.post("/albums", (req, res) => {
   const { title, genre, picture, artist } = req.body;
   connection.query(
@@ -40,7 +69,24 @@ app.post("/albums", (req, res) => {
         console.log(err);
         res.status(500).send("Error saving an album");
       } else {
-        res.status(200).send("Successfully saved");
+        res.status(200).send("Album(s) successfully saved");
+      }
+    }
+  );
+});
+
+// CREATE TRACK(S)
+app.post("/tracks", (req, res) => {
+  const { title, youtube_url, id_album } = req.body;
+  connection.query(
+    "INSERT INTO tracks(title, youtube_url, id_album) VALUES(?, ?, ?)",
+    [title, youtube_url, id_album],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error saving a track");
+      } else {
+        res.status(200).send("Track(s) successfully saved");
       }
     }
   );
