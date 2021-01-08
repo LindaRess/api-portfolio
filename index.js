9507b3b3bb3d8d3486018d7d9f17d56e77d6d190
“Linda <linda.resseguier@gmail.com>
2021-01-08 14:13:35 +0100

    first route get and post for albums working

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const express = require("express");
-const app = express();
+const connection = require("./config");
 
 const port = 3000;
+const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Welcome to Express");
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(request);
+  res.send("Welcome to my favorite albums list");
+});
+
+app.get("/albums", (request, response) => {
+  connection.query("SELECT * from albums", (err, results) => {
+    if (err) {
+      response.status(500).send("Error retrieving albums");
+    } else {
+      response.status(200).json(results);
+    }
+  });
+});
+
+app.post("/albums", (req, res) => {
+  const { title, genre, picture, artist } = req.body;
+  connection.query(
+    "INSERT INTO albums(title, genre, picture, artist) VALUES(?, ?, ?, ?)",
+    [title, genre, picture, artist],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error saving an album");
+      } else {
+        res.status(200).send("Successfully saved");
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
